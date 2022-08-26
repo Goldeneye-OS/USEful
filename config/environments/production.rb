fe6a86b7838c9c3a931de195aecb47a52b3a08df
Lord Goldeneye <c.goldeneye@protonmail.com>
2022-08-26 16:42:51 -0700

    f

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,21 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+	config.action_mailer.perform_deliveries = true
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.default_options = {
+		from: ENV["MAILER_FROM"]
+	}
+  ActionMailer::Base.smtp_settings = {
+		:user_name => ENV["MAILER_USERNAME"],
+		:password => ENV["MAILER_PASSWORD"], 
+		:domain => ENV["MAILER_DOMAIN"],
+		:address => ENV["MAILER_ADDRESS"],
+		:port => ENV["MAILER_PORT"],
+		:authentication => :plain,
+		:enable_starttls_auto => true
+	}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
